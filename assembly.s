8fa3f45612449f0321db415fbf759e4bc83c0119
k11logram <161375355+k11logram@users.noreply.github.com>
2025-08-25 12:02:52 +0200

    Add files via upload

--- assembly.s
+++ assembly.s
@@ -18,122 +18,113 @@ vectors:
 ASM_Main:
 
 	@ Some code is given below for you to start with
-	@ Enabling clock for GPIOA AND GPIOB
-	LDR R0, RCC_BASE  		@ Load the base address of Reset and clock control module from memory to the register R0.
-	LDR R1, [R0, #0x14]     @ Load AHBENR peripheral clock enable register from memory to the register R1.
-	LDR R2, AHBENR_GPIOAB	@ Load AHBENR peripheral clock enable register for GPIOA and GPIOB from memory to the register R2.
-	ORRS R1, R1, R2         @ Logical OR on R1 = R2 + R1 which sets Bit 17 and 18 which enables the clock for port A and B.
-	STR R1, [R0, #0x14]     @ Store register R1 back to memory at memory address 0x40021014.
+	LDR R0, RCC_BASE  		@ Enable clock for GPIOA and B by setting bit 17 and 18 in RCC_AHBENR
+	LDR R1, [R0, #0x14]
+	LDR R2, AHBENR_GPIOAB	@ AHBENR_GPIOAB is defined under LITERALS at the end of the code
+	ORRS R1, R1, R2
+	STR R1, [R0, #0x14]
 
-    @ Enabling Pull up resistors for GPIO A.
-	LDR R0, GPIOA_BASE		@ Load GPIOA_BASE from memory to register R0.
-	MOVS R1, #0b01010101    @ Put 85 on register R1.
-	STR R1, [R0, #0x0C]     @ Store register R1 back to memory at memory address 0x4800000C.
-
-	@ Configuring the mode for GPIO B.
-	LDR R1, GPIOB_BASE  	@ Load GPIOB_BASE from memory to register R1
-	LDR R2, MODER_OUTPUT    @ Load MODER_OUTPUT memory address from memory to register R2
-	STR R2, [R1, #0]        @ Store register R2 on R1 at #0 memory address which is just 0101 0101 0101 0101 the sets PB0 - PB7 to output mode.
-	MOVS R2, #0         	@ Put #0 memory address to register R2 as R2 will be dedicated to holding the value on the LEDs
-    SUBS R2, R2 ,#1         @ Bug Fix
+	LDR R0, GPIOA_BASE		@ Enable pull-up resistors for pushbuttons
+	MOVS R1, #0b01010101
+	STR R1, [R0, #0x0C]
+	LDR R1, GPIOB_BASE  	@ Set pins connected to LEDs to outputs
+	LDR R2, MODER_OUTPUT
+	STR R2, [R1, #0]
+	MOVS R2, #0         	@ NOTE: R2 will be dedicated to holding the value on the LEDs
 
 @ TODO: Add code, labels and logic for button checks and LED patterns
-SW0_Pressed:
-	LDR R4, [R0, #0x10]     @ Load input data register again.
-
-	ADDS R2, R2, #2
-	BL delay_loader_1
-	BL delay_0_7s
-	STR R2, [R1 , #0x14]
-
-	MOVS R5, #0x01
-	TST R4, R5
-    BNE main_loop           @ Branch to main if z flag is not set.
-    B SW0_Pressed           @ Branch to conditon SW0_Pressed if z flag is not set.
-
-SW1_Pressed:
-	LDR R4, [R0, #0x10]
-
-	ADDS R2, R2, #1
-	BL delay_loader_2
-	BL delay_0_3s
-	STR R2, [R1 , #0x14]
-
-	MOVS R5, #0x01
-    TST R4, R5
-    BNE main_loop
-    B SW1_Pressed
-
-SW2_Pressed:
-    LDR R4, [R0, #0x10]
-
-    MOVS R2, #0xAA
-    STR R2, [R1 , #0x14]
-
-    MOVS R5, #0x04
-    TST R4, R5
-    BNE main_loop
-    B SW2_Pressed
-
-SW3_Pressed:
-    LDR R4, [R0, #0x10]
-
-    MOVS R5, #0x08
-    TST R4, R5
-    BNE main_loop
-    B SW3_Pressed
 
 main_loop:
-	 LDR R4, [R0, #0x10]    @ Load input data register from GPIOA to register R4.
-     
-	 MOVS R5, #0x03
-	 TST R4, R5
-	 BEQ SW0_SW1_Pressed
-  
-	 MOVS R5, #0x01         @ Put #0x01 to register R5 which is just a bit mask for PB0.
-	 TST R4, R5             @ Checks whether bit 0 of R4 is 0 if so z flag is set else cleared due to pull up resistors enabled.
-	 BEQ SW0_Pressed        @ if z flag is set the program will branch to label SWO condition else sequence follows.
+	@ Write current LED pattern to the LEDs first
+	MOVS R6, #0xFF        @ Create mask for lower 8 bits
+	ANDS R2, R2, R6       @ Keep only lower 8 bits
+	STR R2, [R1, #0x14]   @ Write to GPIOB_ODR
+	LDR R4, [R0, #0x10] @GPIOA_IDR is at offset 0x10
 
-	 MOVS R5, #0x02
-	 TST R4, R5
-	 BEQ SW1_Pressed
 
-	 MOVS R5, #0x04
-	 TST R4, R5
-	 BEQ SW2_Pressed
+	@ CHECK FOR SWITCH 4 FIRST (both buttons 0 and 1 pressed)
+	MOVS R5, #0b00000011
+	ANDS R5, R4, R5
+	CMP R5, #0b00000000 @ Check if BOTH bits are set
+	BEQ switch_four @ Branch if both switches pressed
 
-	 MOVS R5, #0x08
-	 TST R4, R5
-	 BEQ SW3_Pressed
+	@ we are checking if switch 0 is being pressed
+	MOVS R5,#0b00000001
+	ANDS R5, R4, R5 @ Mask bits 0
+	@CMP R5, #0x00
+	BEQ switch_zero
 
-	 ADDS R2, R2, #1       @ Increments Register R2 ,where R2 = R2+1.
-	 BL delay_loader_1     @ Function Call delay_loader_1.
-	 BL delay_0_7s         @ Function Call delay_0_7s.
-	 B write_leds          @ Store register R2 on R1 at memory address #0x14.
-	 B main_loop           @ Branch back to main.
+	@ we are checking if switch 1 is being pressed
+	MOVS R5,#0b00000010
+	ANDS R5, R4, R5 @ Mask bits 1
+	@CMP R5, #0x00
+	BEQ switch_one
 
-write_leds:
-	STR R2, [R1, #0x14]
+	@ we are checking if switch 2 is being pressed
+	MOVS R5,#0b00000100
+	ANDS R5, R4, R5 @ Mask bits 2
+	@CMP R5, #0x00
+	BEQ switch_two
+
+	@ we are checking if switch 3 is being pressed
+	MOVS R5,#0b00001000
+	ANDS R5, R4, R5 @ Mask bits 3
+	@CMP R5, #0x00
+	BEQ switch_three
+
+
+@ we should increment the LED pattern by 1 every 0.7 seconds as long as no button is being pressed
+default:
+	LDR R3, LONG_DELAY_CNT
+	BL Delay_Loop
+	B case_1
+
+@ Increment LED pattern by 1
+case_1:
+	ADDS R2, R2, #1
 	B main_loop
-delay_loader_1:             @ Function to Load LONG_DELAY_CNT memory address to register.
-	LDR R3, =LONG_DELAY_CNT @ Load LONG_DELAY_CNT memory address from memory to register R3.
-	LDR R3, [R3]            @ Actual value of LONG_DELAY_CNT Loaded to R1
-	BX LR                   @ Links the program back to register.
 
-delay_0_7s:                 @ Delay function which delays program for 0.7s.
-	SUBS R3,R3 ,#1          @ Decrements register R3,where R3 = R3-1.
-	BNE delay_0_7s          @ Checks if register R3 has not reached zero if not branches back to function else program execution continues.
-	BX LR
+@ Increment LED pattern by 2
+case_2:
+	ADDS R2, R2, #2
+	B main_loop
 
-delay_loader_2:             @ Function to Load SHORT_DELAY_CNT memory address to register.
-	LDR R3, =SHORT_DELAY_CNT
-	LDR R3, [R3]
-	BX LR
+@ We should increment the LED pattern by 2 while button 0 is being held down
+switch_zero:
+	LDR R3, LONG_DELAY_CNT
+	BL Delay_Loop
+	B case_2
 
-delay_0_3s:                 @ Delay function which delays program by 0.3s
-	SUBS R3,R3 ,#1
-	BNE delay_0_3s
-	BX LR
+@ change the delay to 0.3 seconds instead of 0.7 seconds while button 1 is being held down
+switch_one:
+	LDR R3, SHORT_DELAY_CNT
+	BL Delay_Loop
+	B case_1
+
+@ change the LED pattern to 0xAA while button 2 is being held down
+switch_two:
+	MOVS R2, #0xAA
+	B main_loop
+
+@ freeze the LED pattern while button 3 is being held down
+switch_three:
+	B main_loop
+
+@ Both buttons 0 and 1 pressed: 0.3 second delay, increment by 2
+switch_four:
+	LDR R3, SHORT_DELAY_CNT
+	BL Delay_Loop
+	ADDS R2, R2, #2    @ Increment by 2 only
+	B main_loop
+
+Delay_Loop:
+	SUBS R3, R3, #1
+	BNE Delay_Loop
+	BX LR @ Return from subroutine
+
+@write_leds:
+@	STR R2, [R1, #0x14]
+@	B main_loop
 
 @ LITERALS; DO NOT EDIT
 	.align
@@ -144,5 +135,5 @@ GPIOB_BASE:  		.word 0x48000400
 MODER_OUTPUT: 		.word 0x5555
 
 @ TODO: Add your own values for these delays
-LONG_DELAY_CNT: 	.word 5600000
-SHORT_DELAY_CNT: 	.word 2400000
+LONG_DELAY_CNT: 	.word 1400000
+SHORT_DELAY_CNT: 	.word 600000
